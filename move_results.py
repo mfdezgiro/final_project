f0931ddf58254c83dfa24d381e7561717f9da47e
mfdezgiro <67413661+mfdezgiro@users.noreply.github.com>
2020-06-25 17:11:50 +0200

    Add files via upload

--- move_results.py
+++ move_results.py
@@ -1,51 +1,56 @@
 def folder_results():
+
 	import shutil
 	import os
 
-	if not os.path.exists("blast_results"):
-		os.mkdir("blast_results")
+	os.mkdir("blast_results")
 
 	dest1 = "blast_results"
 
 	files = os.listdir()
 
 	for f in files:
+
 		if f.startswith("blast_"):
-	   	 shutil.move(f, dest1)
+
+	   		shutil.move(f, dest1)
 
 
-	if not os.path.exists("muscle_results"):
-		os.mkdir("muscle_results")
+	os.mkdir("muscle_results")
 
 	dest2 = "muscle_results"
 
 	files = os.listdir()
 
 	for f in files:
+
 		if f.startswith("muscle_"):
-	   	 shutil.move(f, dest2)
+
+	   		shutil.move(f, dest2)
 
 
 
-	if not os.path.exists("trees"):
-		os.mkdir("trees")
+	os.mkdir("trees")
 
 	dest3 = "trees"
 
 	files = os.listdir()
 
 	for f in files:
+
 		if f.startswith("tree_"):
-	   	 shutil.move(f, dest3)
+
+	   		shutil.move(f, dest3)
 
 
-	if not os.path.exists("domains"):
-		os.mkdir("domains")
+	os.mkdir("domains")
 
 	dest4 = "domains"
 
 	files = os.listdir()
 
 	for f in files:
+
 		if f.startswith("dominios_"):
-	   	 shutil.move(f, dest4)
+
+	   		shutil.move(f, dest4)
